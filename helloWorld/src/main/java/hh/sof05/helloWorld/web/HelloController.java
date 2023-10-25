74f48463c4341b0c21649a709eb100d17d68c6e9
SampoL <sampojolehtonen@gmail.com>
2023-10-25 13:31:12 +0300

    endpoint updated

--- helloWorld/src/main/java/hh/sof05/helloWorld/web/HelloController.java
+++ helloWorld/src/main/java/hh/sof05/helloWorld/web/HelloController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/*")
 	public String hello() {
 		return "hello";
 	}
+
 
 }
