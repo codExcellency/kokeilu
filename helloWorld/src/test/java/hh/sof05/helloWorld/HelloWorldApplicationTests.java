c73ddfdd24f4df2cb2a625b51f187c257d1acbed
SampoL <sampojolehtonen@gmail.com>
2023-10-25 12:00:44 +0300

    eka lisäys

---
+++ helloWorld/src/test/java/hh/sof05/helloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof05.helloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
